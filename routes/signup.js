de2b961453793bc4ead0f545ec750ef80fc9043a
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-22 19:33:16 +0100

    Added post method

--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/user");
+
 router.get("/", (req, res) => {
     res.render("signup");
 });
 
 router.post("/", (req, res) => {
-    res.send(req.body.username);
+    const User = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        subscriptions: [],
+    };
+
+    User.save((err) => {
+        if (err) {
+            res.redirect("/signup");
+        } else {
+            res.render("subscriptions");
+        }
+    });
 });
 
 module.exports = router;
