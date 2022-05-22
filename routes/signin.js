a51cb675eaf9ea90311a1fd1fece49405d092809
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-22 18:40:42 +0100

    Added signin

--- routes/signin.js
+++ routes/signin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("SIGN IN!");
+    res.render("signin");
 });
 
 module.exports = router;
