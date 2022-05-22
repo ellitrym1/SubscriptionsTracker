6ad386222db387c077cb0fb95d0cf66b5e0cb088
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-22 21:40:04 +0100

    Added module export for User model

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(`mongodb://localhost:27017/subscriptionsDB`, {
+        useNewUrlParser: true,
+        family: 4,
+    })
+    .then((db) => console.log("DB is connected"))
+    .catch((err) => console.log(err));
 
 const User = require("../models/user");
 
@@ -8,18 +17,20 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const User = {
+    const user = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         subscriptions: [],
-    };
+    });
 
-    User.save((err) => {
+    user.save((err) => {
         if (err) {
+            console.log(err);
+            alert("FAILED");
             res.redirect("/signup");
         } else {
-            res.render("subscriptions");
+            res.redirect("/subscriptions");
         }
     });
 });
