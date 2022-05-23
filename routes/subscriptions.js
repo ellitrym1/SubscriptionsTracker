b9abc99b43d047fb150bba89fa3ae77a2da8054b
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-23 05:22:30 +0100

    Added Add Subscription

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { route } = require("./home");
 const router = express.Router();
 
 const User = require("../models/user");
+const Subscription = require("../models/subscription");
+const { updateOne } = require("../models/user");
 
 router.get("/", (req, res) => {
     const userID = req.cookies.user;
@@ -13,9 +14,28 @@ router.get("/", (req, res) => {
     }
 });
 
-router.get("/add", (req, res) => {});
+router.get("/add", (req, res) => {
+    res.render("subscription-add");
+});
 
-router.post("/add", (req, res) => {});
+router.post("/add", async (req, res) => {
+    const subscription = {
+        name: req.body.name,
+        paymentDate: req.body.date,
+        paymentFrequency: req.body.frequency,
+        amount: req.body.amount,
+    };
+    console.log(subscription);
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: req.cookies.user },
+        {
+            subscriptions: subscription,
+        },
+        { new: true }
+    );
+    console.log(updatedUser);
+    res.redirect(`/subscriptions`);
+});
 
 router.get("/:userID/view", (req, res) => {
     const userID = req.params.userID;
