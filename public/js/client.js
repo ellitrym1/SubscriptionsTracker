11bae51aa722aa2747e724a764186c7a795dc973
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-25 14:27:49 +0100

    Implemented add function using popup modal

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,21 @@
 let popupBtn = document.querySelector(".popup-close");
 let popupModalBg = document.querySelector(".popup-modal-bg");
 
+let addBtn = document.querySelector(".add-btn");
+let popupModalAddBg = document.querySelector(".popup-modal-add-bg");
+
 let editBtn = document.querySelector(".edit-btn");
 let popupModalEditBg = document.querySelector(".popup-modal-edit-bg");
 
 let deleteBtn = document.querySelector(".delete-btn");
 let popupModalDeleteBg = document.querySelector(".popup-modal-delete-bg");
 
+addBtn.addEventListener("click", function () {
+    popupModalAddBg.classList.add("popup-toggle");
+});
+
 editBtn.addEventListener("click", function () {
-    popupModalBg.classList.add("popup-toggle");
+    popupModalEditBg.classList.add("popup-toggle");
 });
 
 deleteBtn.addEventListener("click", function () {
