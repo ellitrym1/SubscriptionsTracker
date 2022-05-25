895b8235eb9c368e9edb265fb6c066f03ffea17e
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-25 09:17:28 +0100

    Changed button tag in form to div

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,7 @@
 let popupBtn = document.querySelector(".popup-btn");
+let editBtn = document.querySelector(".edit-btn");
 let popupModalBg = document.querySelector(".popup-modal-bg");
 
-popupBtn.addEventListener("click", () => {
+editBtn.addEventListener("click", function () {
     popupModalBg.classList.add("popup-toggle");
 });
