cc48cadfe882a7c96b7645a0c8f2768341f7eed0
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-25 09:31:38 +0100

    Implemented delete function using popup modal

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,21 @@
-let popupBtn = document.querySelector(".popup-btn");
-let editBtn = document.querySelector(".edit-btn");
+let popupBtn = document.querySelector(".popup-close");
 let popupModalBg = document.querySelector(".popup-modal-bg");
+
+let editBtn = document.querySelector(".edit-btn");
+let popupModalEditBg = document.querySelector(".popup-modal-edit-bg");
+
+let deleteBtn = document.querySelector(".delete-btn");
+let popupModalDeleteBg = document.querySelector(".popup-modal-delete-bg");
 
 editBtn.addEventListener("click", function () {
     popupModalBg.classList.add("popup-toggle");
+});
+
+deleteBtn.addEventListener("click", function () {
+    popupModalDeleteBg.classList.add("popup-toggle");
+});
+
+popupBtn.addEventListener("click", function () {
+    console.log("MEOW");
+    popupModalBg.classList.remove("popup-toggle");
 });
