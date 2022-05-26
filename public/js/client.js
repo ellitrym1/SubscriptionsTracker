3f61298f33a590ea1c3bf94ff4551ed73f0abf20
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-05-26 13:59:38 +0100

    Fixed delete/edit button not working for nth subscriptions

--- public/js/client.js
+++ public/js/client.js
@@ -3,22 +3,34 @@ let popupBtn = document.querySelectorAll(".popup-close");
 let addBtn = document.querySelector(".add-btn");
 let popupModalAddBg = document.querySelector(".popup-modal-add-bg");
 
-let editBtn = document.querySelector(".edit-btn");
+let editBtn = document.querySelectorAll(".edit-btn");
 let popupModalEditBg = document.querySelector(".popup-modal-edit-bg");
 
-let deleteBtn = document.querySelector(".delete-btn");
+let deleteBtn = document.querySelectorAll(".delete-btn");
 let popupModalDeleteBg = document.querySelector(".popup-modal-delete-bg");
 
 addBtn.addEventListener("click", function () {
     popupModalAddBg.classList.add("popup-toggle");
 });
+console.log(editBtn);
+// editBtn.addEventListener("click", function () {
+//     popupModalEditBg.classList.add("popup-toggle");
+// });
 
-editBtn.addEventListener("click", function () {
-    popupModalEditBg.classList.add("popup-toggle");
+editBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        popupModalEditBg.classList.add("popup-toggle");
+    });
 });
 
-deleteBtn.addEventListener("click", function () {
-    popupModalDeleteBg.classList.add("popup-toggle");
+// deleteBtn.addEventListener("click", function () {
+//     popupModalDeleteBg.classList.add("popup-toggle");
+// });
+
+deleteBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        popupModalDeleteBg.classList.add("popup-toggle");
+    });
 });
 
 popupBtn[0].addEventListener("click", function () {
